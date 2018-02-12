bc5cf0aecbcee5ee354b4d7cdaeb5f303a8a5337
Dan Allen <dan@opendevise.com>
2018-02-12 03:55:38 -0700

    don't need to wrap pack stream in Promise

--- tasks/pack.js
+++ tasks/pack.js
@@ -4,11 +4,7 @@ const vfs = require('vinyl-fs')
 const zip = require('gulp-vinyl-zip')
 
 module.exports = async (src, dest, bundleName) =>
-  new Promise((resolve, reject) =>
-    vfs
-      .src('**/*', { base: src, cwd: src })
-      .pipe(zip.zip(`${bundleName}-bundle.zip`))
-      .pipe(vfs.dest(dest))
-      .on('error', reject)
-      .on('end', resolve)
-  )
+  vfs
+    .src('**/*', { base: src, cwd: src })
+    .pipe(zip.zip(`${bundleName}-bundle.zip`))
+    .pipe(vfs.dest(dest))

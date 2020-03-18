01f11f6fcf749a5ee740c46b86a010ccc2d2b861
Dan Allen <dan@opendevise.com>
2020-03-18 16:02:54 -0600

    register built-in helpers (resolvePage and resolvePageURL) with stub implementation in preview site

--- gulp.d/tasks/build-preview-pages.js
+++ gulp.d/tasks/build-preview-pages.js
@@ -79,6 +79,8 @@ function registerPartials (src) {
 }
 
 function registerHelpers (src) {
+  handlebars.registerHelper('resolvePage', resolvePage)
+  handlebars.registerHelper('resolvePageURL', resolvePageURL)
   return vfs.src('helpers/*.js', { base: src, cwd: src }).pipe(
     map((file, enc, next) => {
       handlebars.registerHelper(file.stem, requireFromString(file.contents.toString()))
@@ -98,6 +100,14 @@ function compileLayouts (src, layouts) {
 
 function copyImages (src, dest) {
   return vfs.src('**/*.{png,svg}', { base: src, cwd: src }).pipe(vfs.dest(dest))
+}
+
+function resolvePage (spec, context = {}) {
+  if (spec) return { pub: { url: resolvePageURL(spec) } }
+}
+
+function resolvePageURL (spec, context = {}) {
+  if (spec) return '/' + (spec = spec.split(':').pop()).slice(0, spec.lastIndexOf('.')) + '.html'
 }
 
 function toPromise (stream) {

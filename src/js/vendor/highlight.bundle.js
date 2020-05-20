9424c131603fd19632f4755c13339e5794e51607
Dan Allen <dan@opendevise.com>
2020-05-20 00:38:12 -0600

    remove commented lines in highlight.js bundle

--- src/js/vendor/highlight.bundle.js
+++ src/js/vendor/highlight.bundle.js
@@ -2,7 +2,6 @@
   'use strict'
 
   var hljs = require('highlight.js/lib/highlight')
-  //hljs.registerLanguage('apache', require('highlight.js/lib/languages/apache'))
   hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
   hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
   hljs.registerLanguage('clojure', require('highlight.js/lib/languages/clojure'))
@@ -14,15 +13,12 @@
   hljs.registerLanguage('go', require('highlight.js/lib/languages/go'))
   hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
   hljs.registerLanguage('haskell', require('highlight.js/lib/languages/haskell'))
-  //hljs.registerLanguage('http', require('highlight.js/lib/languages/http'))
-  //hljs.registerLanguage('ini', require('highlight.js/lib/languages/ini'))
   hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
   hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
   hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
   hljs.registerLanguage('kotlin', require('highlight.js/lib/languages/kotlin'))
   hljs.registerLanguage('makefile', require('highlight.js/lib/languages/makefile'))
   hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
-  //hljs.registerLanguage('nginx', require('highlight.js/lib/languages/nginx'))
   hljs.registerLanguage('nix', require('highlight.js/lib/languages/nix'))
   hljs.registerLanguage('objectivec', require('highlight.js/lib/languages/objectivec'))
   hljs.registerLanguage('perl', require('highlight.js/lib/languages/perl'))

43624a6bcfa492fcdc28db30812c4e6ae7b293a6
Dan Allen <dan@opendevise.com>
2020-05-20 00:38:58 -0600

    add elixir language to highlight.js bundle

--- src/js/vendor/highlight.bundle.js
+++ src/js/vendor/highlight.bundle.js
@@ -10,6 +10,7 @@
   hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
   hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'))
   hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
+  hljs.registerLanguage('elixir', require('highlight.js/lib/languages/elixir'))
   hljs.registerLanguage('go', require('highlight.js/lib/languages/go'))
   hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
   hljs.registerLanguage('haskell', require('highlight.js/lib/languages/haskell'))

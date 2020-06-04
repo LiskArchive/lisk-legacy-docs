7e292535235bc4c9d1d65ed0eb012ff2b1a0f9a0
Mona Baerenfaenger <mona@lightcurve.io>
2020-06-04 14:15:07 +0200

    Update UI/UX of docs

---
+++ _/js/vendor/top-versions.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var t,e=document.querySelector(".top-versions .version-menu-toggle");e&&(t=document.querySelector(".top-versions"),e.addEventListener("click",function(e){t.classList.toggle("is-active"),e.stopPropagation()}),document.documentElement.addEventListener("click",function(){t.classList.remove("is-active")}))}();

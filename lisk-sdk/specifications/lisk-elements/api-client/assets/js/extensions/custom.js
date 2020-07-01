d141b95a82c873a615cf6033de5839427a7490d3
Mona Baerenfaenger <mona@lightcurve.io>
2020-07-01 11:48:35 +0200

    Test typedocs

--- lisk-sdk/specifications/lisk-elements/api-client/assets/js/extensions/custom.js
+++ lisk-sdk/specifications/lisk-elements/api-client/assets/js/extensions/custom.js
@@ -2,4 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var link = document.getElementById("backlink").href;
     console.log("+++++++++++LINK++++++++++++");
     console.log(link);
+    var linkArray = link.split('/');
+    linkArray.slice
+    let newArray = linkArray.slice(0, 6);
+    var newLink = newArray.join();
+    console.log("+++++++++++NEW LINK++++++++++++");
+    console.log(newLink);
+    document.getElementById("backlink").href = newLink;
 })

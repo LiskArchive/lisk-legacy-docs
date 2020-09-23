7e5ad5aa3fb373b4a9c5959406afc42e490cd01a
Mona Baerenfaenger <mona@lightcurve.io>
2020-09-23 11:45:00 +0200

    Test docs ui

--- _/js/vendor/custom.js
+++ _/js/vendor/custom.js
@@ -1,1 +1,1 @@
-!function(){"use strict";var t=document.querySelectorAll(".nav-item:not(.single-page)");console.log(t[1].childNodes),"Explanations"===t[1].childNodes[1].textContent?t[2].classList.add("is-active"):t[1].classList.add("is-active")}();
+!function(){"use strict";var t=document.querySelectorAll(".nav-item:not(.single-page)");"Explanations"===t[1].childNodes[3].textContent?t[2].classList.add("is-active"):t[1].classList.add("is-active")}();

2976b7cec30db2ada25724947a20332c349eac09
Mona Baerenfaenger <mona@lightcurve.io>
2020-10-14 10:18:07 +0200

    Increase docs content width

--- _/js/vendor/custom.js
+++ _/js/vendor/custom.js
@@ -1,1 +1,1 @@
-!function(){"use strict";document.querySelectorAll(".nav-item:not(.single-page)")[1].classList.add("is-active")}();
+!function(){"use strict";var t=document.querySelectorAll(".nav-item:not(.single-page)");"Explanations"===t[1].childNodes[3].textContent?t[2].classList.add("is-active"):t[1].classList.add("is-active")}();

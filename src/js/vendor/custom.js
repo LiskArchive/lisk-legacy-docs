a0ab876af37b71dfccf929e3a4239409c2da25dd
Mona Baerenfaenger <mona@lightcurve.io>
2020-12-01 11:44:36 +0100

    UI Updates

--- src/js/vendor/custom.js
+++ src/js/vendor/custom.js
@@ -3,12 +3,13 @@
 
   var navList = document.querySelectorAll('.nav-item:not(.single-page)');
 
-  if (navList[1].childNodes[3].textContent === "Explanations") {
+  /*if (navList[1].childNodes[3].textContent === "Explanations") {
     navList[2].classList.add("is-active")
   } else {
     navList[1].classList.add("is-active");
-  }
+  }*/
 
+  navList[1].classList.add("is-active");
 
 })()
 

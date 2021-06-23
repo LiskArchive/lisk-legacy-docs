8a35e92e09f9c099aae94c4ea9d36499a2f600ad
Mona Baerenfaenger <mona@lightcurve.io>
2021-06-23 11:21:35 +0200

    Add image zoom

--- _/js/vendor/custom.js
+++ _/js/vendor/custom.js
@@ -1,1 +1,1 @@
-!function(){"use strict";document.querySelectorAll(".nav-item:not(.single-page)")[1].classList.add("is-active")}();
+!function(){"use strict";document.querySelectorAll(".nav-item:not(.single-page)")[1].classList.add("is-active");for(var e=document.querySelectorAll("img"),t=0;t<e.length;t++)e[t].setAttribute("data-zoomable","true");mediumZoom("[data-zoomable]")}();

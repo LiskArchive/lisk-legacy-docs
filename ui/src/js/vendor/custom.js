df98fb392285ab60f06a67c9228bc5d211d1d277
Tschakki <tschakki@posteo.de>
2022-01-27 14:08:15 +0100

    Exclude copy-icon from image-zoom

--- ui/src/js/vendor/custom.js
+++ ui/src/js/vendor/custom.js
@@ -4,7 +4,9 @@
   var images = document.querySelectorAll('img')
 
   for (var i = 0; i < images.length; i++) {
-    images[i].setAttribute('data-zoomable', 'true')
+    if (!images[i].classList.contains('copy-icon')){
+      images[i].setAttribute('data-zoomable', 'true')
+    }
   }
 
   mediumZoom('[data-zoomable]')

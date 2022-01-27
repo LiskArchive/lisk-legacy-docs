867eb1f07d51b9dd3abc8c36a915b1509511b5c5
Tschakki <tschakki@posteo.de>
2022-01-27 23:57:25 +0100

    Clean up js

--- ui/src/js/vendor/custom.js
+++ ui/src/js/vendor/custom.js
@@ -27,77 +27,17 @@ if(theme && theme == 'dark') {
   themelink.setAttribute('href', uiRootPath+'/css/site.css');
   themetoggle.classList.remove("dark");
 }
-checkBox.checked = theme == 'dark' ? true : false;
 
 function toggleTheme() {
-  // Obtains an array of all <link>
-  // elements.
-  // Select your element using indexing.
   let theme = document.getElementById('theme');
 
-
-  // Change the value of href attribute
-  // to change the css sheet.
   if (theme.getAttribute('href') == uiRootPath+'/css/site.css') {
     theme.setAttribute('href', uiRootPath+'/css/dark-site.css');
     window.localStorage.setItem('data-theme', 'dark');
+    themetoggle.classList.add("dark");
   } else {
     theme.setAttribute('href', uiRootPath+'/css/site.css');
     window.localStorage.setItem('data-theme', 'light');
+    themetoggle.classList.remove("dark");
   }
-}
-
-/*let themetoggle = document.getElementById("theme-toggle");
-
-document
-  .querySelector(`link[title="Dark"]`)
-  .setAttribute("disabled", "disabled");
-
-themetoggle.onclick = function(){
-  console.log("clicked!");
-  themetoggle.classList.toggle("dark");
-
-  if (themetoggle.classList.contains("dark")) {
-    console.log("if!");
-    document
-      .querySelector(`link[title="Dark"]`)
-      .removeAttribute("disabled");
-    document
-      .querySelector(`link[title="Default"]`)
-      .setAttribute("disabled", "disabled");
-  } else {
-    console.log("else!");
-    document
-      .querySelector(`link[title="Default"]`)
-      .removeAttribute("disabled");
-    document
-      .querySelector(`link[title="Dark"]`)
-      .setAttribute("disabled", "disabled");
-  }
-};*/
-
-/*
-console.log(themetoggle);
-
-themetoggle.click(function () {
-  console.log("clicked!");
-  $(this).classList.toggle("dark");
-
-  console.log($(this).classList);
-  if ($(this).classList.contains("dark")) {
-    document
-      .querySelector(`link[title="Default"]`)
-      .removeAttribute("disabled");
-    document
-      .querySelector(`link[title="Dark"]`)
-      .setAttribute("disabled", "disabled");
-  } else {
-    document
-      .querySelector(`link[title="Dark"]`)
-      .removeAttribute("disabled");
-    document
-      .querySelector(`link[title="Default"]`)
-      .setAttribute("disabled", "disabled");
-  }
-});
-*/
+};

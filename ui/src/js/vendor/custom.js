4d0b7bd4f45e58493b62c69756911564c915592f
Tschakki <tschakki@posteo.de>
2022-01-27 23:08:40 +0100

    Add dark theme toggle

--- ui/src/js/vendor/custom.js
+++ ui/src/js/vendor/custom.js
@@ -11,3 +11,54 @@
 
   mediumZoom('[data-zoomable]')
 })()
+
+let themetoggle = document.getElementById("theme-toggle");
+
+themetoggle.onclick = function(){
+  console.log("clicked!");
+  themetoggle.classList.toggle("dark");
+
+  if (themetoggle.classList.contains("dark")) {
+    console.log("if!");
+    document
+      .querySelector(`link[title="Dark"]`)
+      .removeAttribute("disabled");
+    document
+      .querySelector(`link[title="Default"]`)
+      .setAttribute("disabled", "disabled");
+  } else {
+    console.log("else!");
+    document
+      .querySelector(`link[title="Default"]`)
+      .removeAttribute("disabled");
+    document
+      .querySelector(`link[title="Dark"]`)
+      .setAttribute("disabled", "disabled");
+  }
+};
+
+/*
+console.log(themetoggle);
+
+themetoggle.click(function () {
+  console.log("clicked!");
+  $(this).classList.toggle("dark");
+
+  console.log($(this).classList);
+  if ($(this).classList.contains("dark")) {
+    document
+      .querySelector(`link[title="Default"]`)
+      .removeAttribute("disabled");
+    document
+      .querySelector(`link[title="Dark"]`)
+      .setAttribute("disabled", "disabled");
+  } else {
+    document
+      .querySelector(`link[title="Dark"]`)
+      .removeAttribute("disabled");
+    document
+      .querySelector(`link[title="Default"]`)
+      .setAttribute("disabled", "disabled");
+  }
+});
+*/

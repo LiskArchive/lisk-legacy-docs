9e1fafb72e19609d91ce501ac3d891dc5a502ce3
Tschakki <tschakki@posteo.de>
2022-05-17 11:16:23 +0200

    Test docs

--- _/js/vendor/top-versions.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var t,e=document.querySelector(".top-versions .version-menu-toggle");e&&(t=document.querySelector(".top-versions"),e.addEventListener("click",function(e){t.classList.toggle("is-active"),e.stopPropagation()}),document.documentElement.addEventListener("click",function(){t.classList.remove("is-active")}))}();
